b0c8899f6c23af01c41e2c88a77b7effdbd0ff6d
Simon <simon.dinis@univ-rouen.fr>
2025-09-02 16:04:24 +0200

    fin de séance, reprendre test exposant négatif

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,7 +1,9 @@
 def puissance(a,b):    
-    if (not type(a) or (not type(b))):
+    if (not type(a) is int) or (not type(b) is int):
         raise TypeError("Integers only")
         exit()
+    if a == 0 and b <= 0:
+        raise ValueError("Undefined exposant")
     res = a ** b
     print(f"Le résultat de {a} à l'exposant {b} est {res}")
     return res
--- TP1/test.py
+++ TP1/test.py
@@ -11,3 +11,10 @@ def test_2():
     assert f.puissance(-1,-1) == -1
     assert f.puissance(-1,-2) == 1
     assert f.puissance(-2,-1) == -0.5
+
+def test_3():
+    assert f.puissance(0,12) == 0
+
+def test_4():
+    with pytest.raises(ValueError, match="Undefined exposant"):
+    
