94aba4f59f19176e3d00378deba323e0f0f88ef2
Simon <simon.dinis@univ-rouen.fr>
2025-09-11 16:01:32 +0200

    création de Dockerfile2, déplacement de app_1.py et index.html dans /srv

--- TP4/build_image.sh
+++ TP4/build_image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -f Dockerfile1 -t im-tp4 . 1>/dev/null 2>/dev/null
+docker build -f Dockerfile2 -t im-tp4-2 .
--- TP4/run_app.sh
+++ TP4/run_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run --rm  \
   -p 5000:5000  \
+  --mount type=bind,src=/TP4/srv,dst=/srv  \
   --name tp4-app  \
   --network net-tp4  \
-  im-tp4  \
+  im-tp4-2  \
